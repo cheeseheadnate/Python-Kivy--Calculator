5cc55a93cf4deb1b5e8e572b7207c5d2f0a7874e
cheeseheadnate <cheeseheadnate@gmail.com>
2025-03-10 06:59:30 -0500

    Completed & full functional

--- main.py
+++ main.py
@@ -20,8 +20,12 @@ class Calculator(BoxLayout):
             readonly=True,
             halign="right",
             multiline=False,
+            background_color=[0.2, 0.2, 0.2, 1],
+            foreground_color=[255, 165, 0, 1]
         )
         self.add_widget(self.result)
+        
+        
         
         # Create the buttons
         buttons = [
@@ -39,12 +43,52 @@ class Calculator(BoxLayout):
                 button = Button(
                     text=item,
                     font_size=32,
-                    
+                    background_color=self.set_button_color(item),
+                    on_press=self.button_click
                 )
                 grid.add_widget(button)
                 
         self.add_widget(grid)
+        
+    def set_button_color(self, label):
+        if label in { 'C', '+/-', '%' }:
+            return [0.6, 0.6, 0.6, 1]
+        elif label in { '/', '*', '-', '+', '=' }:
+            return [1, 0.65, 0, 1]
+        return [0.3, 0.3, 0.3, 1]
                     
+    def button_click(self, instance):
+        text = instance.text
+        
+        if text == "C":
+            self.result.text = ""
+        elif text == "=":
+            self.calculate()
+        elif text == "+/-":
+            self.toggle_neg()
+        elif text == "%":
+            self.convert_percent()
+        else: 
+            self.result.text += text
+            
+    
+    # Calculate
+    def calculate(self):
+        try:
+            self.result.text = str(eval(self.result.text)) # corrected
+        except Exception:
+            self.result.text = "ERROR!"
+            
+    def toggle_neg(self):
+        if self.result.text:
+            self.result.text = self.result.text[1:] if self.result.text[0] == "-" else "-" + self.result.text
+            
+    def convert_percent(self):
+        try:
+            self.result.text = str(float(self.result.text)/100)
+        except ValueError:
+            self.result.text = "ERROR!"
+    
     
 class CalculatorApp(App):
     def build(self):
@@ -52,5 +96,3 @@ class CalculatorApp(App):
     
 if __name__ == "__main__":
     CalculatorApp().run()
-    
-    
